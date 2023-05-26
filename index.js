904f94b9d16a2d04945181e3da827ac954ad8240
Mohammadmahdi Moradzadeh <126528880+Mahdi-Moradzadeh@users.noreply.github.com>
2023-05-26 11:03:06 +1000

    final dev

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-class Logo {
-    constructor (text, colour,shape)
-    {
-        this.text = text;
-        this.colour = colour;
-        this.shape = shape;
-        this.height = 300;
-        this.width = 200;
-    }
-    logoCreater (text, colour,shape, height, width){
-        return `<svg version="1.1" width=${width} height=${height} xmlns="http://www.w3.org/2000/svg">
-        <${shape} cx="150" cy="100" r="80" fill=${colour} />
-        <${text} x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-        </svg>`;
-    }
-}
+const shape = require('./lib/shapes.js');
 
-// WHEN I am prompted for text
 function promptUser() {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'text',
-            message: 'What is the text for your logo?',
+            message: 'What is the text for your logo?'
         },
         {
             type: 'input',
-            name: 'textColor',
-            message: 'What color would you like your text to be?',
+            name: 'textColour',
+            message: 'What color would you like your text to be?'
         },
         {
             type: 'rawlist',
@@ -38,19 +22,25 @@ function promptUser() {
         },
         {
             type: 'input',
-            name: 'shapeColor',
-            message: 'What color would you like your shape to be?',
+            name: 'colour',
+            message: 'What color would you like your shape to be?'
         },
-    ]);
+    ])
+    .then((answers) => {
+        const newShape = new shape(answers.text, answers.textColour, answers.colour, answers.shape);
+        const svg = newShape.logoCreater(answers.text, answers.textColour, answers.colour, answers.shape, newShape.width, newShape.height);
+        writetoFile(`logo.svg`,svg);
+    });
 }
 
-const logoDesign = new Logo(promptUser.text,promptUser.colour,promptUser.shape);
 
+function writetoFile (filename, data){
+    fs.writeFile(filename, data, (err) => {
+        if (err){
+            throw err;
+        }
+        console.log('Generated logo.svg');
+    });
+}
 
-fs.writeFile('logo.svg', 'utf8', (err) => {
-    
-    if (err) throw err;
-    logoDesign.logoCreater();
-    console.log('The file has been saved!');
-
-});
+promptUser();
